7e18a6839a61a7d774fb87f20a5c2668623908b1
Betty Anderson <101119141+bettyand@users.noreply.github.com>
2022-07-27 14:22:42 -0400

    basic registration/login w mysql
    https://www.codejava.net/frameworks/spring-boot/user-registration-and-login-tutorial

---
+++ src/main/java/com/example/security/service/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.example.security.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.example.security.model.User;
+import com.example.security.repository.UserRepository;
+
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new CustomUserDetails(user);
+    }
+}
